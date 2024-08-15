a7410200c27c84db3d10f935808a863fa8c4e6c4
Elliot Escovitch <dev.escovitch.ri@gmail.com>
2024-08-14 20:14:14 -0500

    add banner

--- src/main/java/com/eureka/EurekaApplication.java
+++ src/main/java/com/eureka/EurekaApplication.java
@@ -1,5 +1,7 @@
 package com.eureka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,8 +10,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(EurekaApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(EurekaApplication.class, args);
+		logger.info("Welcome to Eureka service!");
 	}
 
 }
